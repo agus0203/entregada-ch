23e9e6627518ba544bc80a776b669219557e8cd7
agus0203 <agus.ranixe@gmail.com>
2022-06-27 02:59:58 -0300

    entrega ch

--- entrega/urls.py
+++ entrega/urls.py
@@ -1,12 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from Proyecto.views import saludo #Para acceder a la vista hay que importar el modulo y el método
+from Proyecto.views import saludo 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', saludo),   #ojo la url no hace falta que se llame saludo/ el nombre es libre, 
-                                #pero la vista sí debe llamarse por su nombre
+    path('', saludo),   
 
    
     
